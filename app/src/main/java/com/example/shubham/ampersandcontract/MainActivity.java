a3cabc8be26338dc55ff6c951ea4960185f871f6
Shubham <vshubhamr@ymail.com>
2019-05-18 17:29:44 +0000

    Sign up Working

--- app/src/main/java/com/example/shubham/ampersandcontract/MainActivity.java
+++ app/src/main/java/com/example/shubham/ampersandcontract/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.shubham.ampersandcontract;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    Button register;
+    EditText email, password;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        register = findViewById(R.id.register);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+
+        try {
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            });
+        }catch (Exception e){ }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+//        updateUI(currentUser);
     }
 }
